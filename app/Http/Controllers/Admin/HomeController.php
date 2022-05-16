bf3f7d0ddd8ff0cf71877046c9707d6805981e58
Ihsan Devs <ichsanhafizdalfajry@gmail.com>
2022-05-16 20:40:40 +0700

    Update HomeController.php

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -9,37 +9,37 @@ class HomeController
     public function index()
     {
         $settings1 = [
-            'chart_title'           => 'Major Report',
-            'chart_type'            => 'bar',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\Major',
-            'group_by_field'        => 'created_at',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '30',
+            'chart_title' => 'Statistik Data Jurusan',
+            'chart_type' => 'line',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\Major',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'aggregate_function' => 'count',
+            'filter_field' => 'created_at',
+            'filter_days' => '30',
             'group_by_field_format' => 'Y-m-d H:i:s',
-            'column_class'          => 'col-md-6',
-            'entries_number'        => '5',
-            'translation_key'       => 'major',
+            'column_class' => 'col-md-6',
+            'entries_number' => '5',
+            'translation_key' => 'major',
         ];
 
         $chart1 = new LaravelChart($settings1);
 
         $settings2 = [
-            'chart_title'           => 'School Report',
-            'chart_type'            => 'line',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\School',
-            'group_by_field'        => 'created_at',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '30',
+            'chart_title' => 'Statistik Data Sekolah',
+            'chart_type' => 'line',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\School',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'aggregate_function' => 'count',
+            'filter_field' => 'created_at',
+            'filter_days' => '30',
             'group_by_field_format' => 'Y-m-d H:i:s',
-            'column_class'          => 'col-md-6',
-            'entries_number'        => '5',
-            'translation_key'       => 'school',
+            'column_class' => 'col-md-6',
+            'entries_number' => '5',
+            'translation_key' => 'school',
         ];
 
         $chart2 = new LaravelChart($settings2);
